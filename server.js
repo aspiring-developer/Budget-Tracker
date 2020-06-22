fe5a0821e8b6255ebce2d367d868f5fd2b23320c
Shi Sha <emailtotheowner@gmail.com>
2020-06-21 22:26:16 -0400

    Ran Lighthouse audit, compressed js codes, created dist and index.js.

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+
+const PORT = 5000;
 
 const app = express();
 
@@ -24,5 +25,5 @@ mongoose.connect("mongodb://localhost/budget", {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port http://localhost:${PORT}`);
 });
