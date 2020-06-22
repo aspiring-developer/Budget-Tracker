5866c628287d28952413a796701c6af1cda58951
Shi Sha <emailtotheowner@gmail.com>
2020-06-22 12:26:33 -0400

    Setting done for deployment.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(logger("dev"));
@@ -12,10 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+ mongoose.connect(MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false});
 
 // routes
 app.use(require("./routes/api.js"));
