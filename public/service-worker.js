ca1af5ad9bd544cd0fdd71a9479897c917aeedc4
Shi Sha <emailtotheowner@gmail.com>
2020-06-22 11:00:06 -0400

    Service worker working.

---
+++ public/service-worker.js
@@ -0,0 +1,37 @@
+const cacheName = 'v1';
+
+const cacheAssets = [
+  'index.html', 'index.js', '/js/main.js', '/icons/icon-192x192.png', '/icons/icon-512x512.png'
+];
+
+// Setting event listener for install
+self.addEventListener('install', evt => {
+  console.log('Files were pre-cached successfully!');
+  evt.waitUntil(
+    caches
+    .open(cacheName)
+    .then(cache => {
+     console.log('Service Worker: Caching now!');
+      cache.addAll(cacheAssets);
+    })
+    .then(() => self.skipWaiting())
+  );
+});
+
+ // Setting event listener for activate
+ self.addEventListener('activate', evt => {
+  console.log('Service Worker activated!');
+   evt.waitUntil(
+   caches.keys().then(cacheNames => {
+       return Promise.all(
+        cacheNames.map(cache => {
+           if (cache !== cacheName) {
+             console.log('Removing old cache data');
+             return caches.delete(cache);
+           }
+         })
+       );
+     })
+   );
+  //  self.clients.claim();
+ });
